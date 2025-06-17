f7f1a79dcefc3a61d556e809acbae0e862c5ca3a
Tebogo Adam Mphahlele <tebogomphahlele12345@gmail.com>
2025-06-17 23:38:52 +0200

    update

--- SWP Project/gunicorn.conf.py
+++ SWP Project/gunicorn.conf.py
@@ -1,4 +1,8 @@
+import os
+
+# Core configuration
 workers = 4
-bind = '0.0.0.0:8000'
+bind = '0.0.0.0:' + os.getenv('PORT', '8000')
 timeout = 120
-worker_class = 'sync'
+keepalive = 5
+worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
